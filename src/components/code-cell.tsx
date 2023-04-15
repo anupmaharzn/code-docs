ec97c9326e2d86772e556935ebb289378f2db947
Anup Maharjan <anup.maharjan@gurzu.net>
2023-04-15 18:54:00 +0545

    added draggable resizable component- error handling

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,31 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CodeEditor from "../components/code-editor";
 import Preview from "../components/preview";
 import bundle from "../bundler/index";
+import Resizable from "./resizable";
 const CodeCell = () => {
   const [input, setInput] = useState("");
+  const [err, setErr] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = async () => {
-    //send rawcode for bundling
-    const output = await bundle(input);
-    // set the bundle output value
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      //send rawcode for bundling
+      const output = await bundle(input);
+      // set the bundle output value
+      setCode(output.code);
+      //set error if any
+      setErr(output.err);
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
-    <div>
-      {/* receive raw code form editor */}
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
+    <Resizable direction="vertical">
+      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+        {/* receive raw code form editor */}
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+
+        {/* send bundled code for preview */}
+        <Preview code={code} err={err} />
       </div>
-      {/* send bundled code for preview */}
-      <Preview code={code} />
-    </div>
+    </Resizable>
   );
 };
 
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,21 +1,32 @@
 import React, { useEffect, useRef } from "react";
+import "./preview.css";
 type PreviewProps = {
   code: string;
+  err: string;
 };
 
 const html = `
     <html>
-      <head></head>
+      <head>
+      <style>html {background-color:white;}</style>
+      </head>
       <body>
         <div id="root"></div>
         <script>
+        const handleError =(err) =>{
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+          console.error(err);
+        }
+        window.addEventListener('error',(event)=>{
+          event.preventDefault();
+          handleError(event.error)
+        })
           window.addEventListener('message', (event) => {
             try {
               eval(event.data);
             } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
+              handleError(err);
             }
           }, false);
         </script>
@@ -23,20 +34,25 @@ const html = `
     </html>
   `;
 //receive the bundled code
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
   const iframe = useRef<any>();
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, "*");
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, "*");
+    }, 50);
   }, [code]);
 
   return (
-    <iframe
-      title="preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+      {err && <div className="preview-error">{err}</div>}
+    </div>
   );
 };
 
